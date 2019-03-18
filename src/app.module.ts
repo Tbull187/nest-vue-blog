d08fc2a84d0ab0f4d1b23916ed927d3cd41e4a01
Tristan <tristan.tbull@gmail.com>
2019-03-18 16:58:07 -0700

    begin working on authService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,15 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BlogModule } from './modules/blog/blog.module';
 import { AuthModule } from './modules/authentication/auth.module';
+import { AuthController } from './modules/authentication/auth.controller';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-blog', { useNewUrlParser: true }),
     BlogModule,
-    AuthModule
+    // AuthModule
   ],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
